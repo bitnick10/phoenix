40aeefcd9ac5bf0a1c54256620171948000d0522
Ginseng <bitnick10@gmail.com>
2015-03-12 13:08:15 +0800

    c

--- Image.h
+++ Image.h
@@ -3,8 +3,20 @@
 template<typename T>
 struct RGB {
     T r, g, b;
+    RGB() {
+    }
+    RGB(int r, int g, int b) {
+        this->r = r;
+        this->g = g;
+        this->b = b;
+    }
+    bool operator==(const RGB<T>& rhs) const {
+        return this->r == rhs.r && this->g == rhs.g && this->b == rhs.b;
+    }
 };
-
+//bool operator==(const RGB<unsigned char>& lhs, const RGB<unsigned char>& rhs) {
+//    return lhs.r == rhs.r && lhs.g == rhs.g && lhs.b == rhs.b;
+//}
 template<typename T>
 class Image {
     unsigned int width_, height_;
@@ -22,8 +34,11 @@ public:
     void set_height(unsigned int height) {
         height_ = height;
     }
-    void set_pixel(unsigned int x, unsigned int y, T& color) {
-        data_[y * height() + x] = color;
+    void SetPixel(unsigned int x, unsigned int y, T& color) {
+        data_[y * width() + x] = color;
+    }
+    T GetPixel(unsigned int x, unsigned int y) {
+        return data_[y * width() + x];
     }
     Image(const char* filename) {
         FIBITMAP *dib = FreeImage_Load(FIF_PNG, filename, PNG_DEFAULT);
@@ -38,12 +53,12 @@ public:
                 for(unsigned int y = 0; y < this->height(); y++) {
                     for(unsigned int x = 0; x < this->width(); x++) {
                         RGBQUAD color;
-                        FreeImage_GetPixelColor(dib, 0, 0, &color);
+                        FreeImage_GetPixelColor(dib, x, y, &color);
                         RGB<unsigned char> c;
                         c.r = color.rgbRed;
                         c.g = color.rgbGreen;
                         c.b = color.rgbBlue;
-                        set_pixel(x, y, c);
+                        SetPixel(x, y, c);
                     }
                 }
             } else {
@@ -51,6 +66,25 @@ public:
             }
         } else {
             throw ;
+        }
+        FreeImage_Unload(dib);
+    }
+    void Save(const char* filename) {
+        FIBITMAP* dib = FreeImage_Allocate(width(), height(), 24);
+        if(typeid(T) == typeid(RGB<unsigned char>)) {
+            for(unsigned int y = 0; y < this->height(); y++) {
+                for(unsigned int x = 0; x < this->width(); x++) {
+                    RGBQUAD color;
+                    RGB<unsigned char> colorXY = GetPixel(x, y);
+                    color.rgbRed = colorXY.r;
+                    color.rgbGreen = colorXY.g;
+                    color.rgbBlue = colorXY.b;
+                    FreeImage_SetPixelColor(dib, x, y, &color);
+                }
+            }
+            FreeImage_Save(FIF_PNG, dib, filename);
+        } else {
+            throw;
         }
         FreeImage_Unload(dib);
     }
